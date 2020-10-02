168762e688e762b903c2241a7648000cc9cc3d6b
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-02 15:48:45 -0400

    testing

--- pointcp/Exercice2.java
+++ pointcp/Exercice2.java
@@ -1,15 +1,22 @@
+import java.util.*;
+
 public class Exercice2 {
 //Char[] charArray = new Char[];
 
     
     public static void main (String args[]){
             
-        ArrayList<Char> charArrayList = new ArrayList<Char>();
+        ArrayList<Character> charArrayList = new ArrayList<Character>();
         Random rand = new Random();
         long start = System.nanoTime();
             
         while((System.nanoTime()- start)/1000000000<10){
-            charArrayList.push((char) (rand.nextInt(26)+'a'));
+            charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a'))); 
         }
+
+        int x = charArrayList.size();
+        System.out.println("Size : " + Integer.toString(x));
+        System.out.println(charArrayList.get(1));
     }
-}
+}//105136606
+//105136606
