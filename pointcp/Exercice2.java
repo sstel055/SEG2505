4de44ad17fec7ca3831da2489be837e89d7a54e7
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-02 16:45:36 -0400

    git ignore

--- pointcp/Exercice2.java
+++ pointcp/Exercice2.java
@@ -10,23 +10,37 @@ public class Exercice2 {
         Random rand = new Random();
         long start = System.nanoTime();
 
-        while((System.nanoTime() - start)/1000000000<10){
-            charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a'))); 
-        }//determine the length of the array to fill it for 10 seconds 
-        int x = charArrayList.size();
-        charArrayList.clear();
+        // while((System.nanoTime() - start)/1000000000<10){
+        //     charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a'))); 
+        // }//determine the length of the array to fill it for 10 seconds 
+        // int x = charArrayList.size();
+        // charArrayList.clear();
 
-        int i = 0;
-        double time = 0.0;
         start = System.nanoTime();    
-        while(i <= x){
-            charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a'))); 
-            time = System.nanoTime() - start;
-            i++;
+        for(int i = 0; i<= 100000000;i++){
+            charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a')));
         }
         long end = System.nanoTime();
         
-        System.out.println(x);
+        //System.out.println(x);
+        System.out.println((end-start)/1000000000);
+
+        start = System.nanoTime(); 
+        for(int i = 0; i<= 100000000;i++){
+            charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a')));
+        }
+        end = System.nanoTime();
+        
+        //System.out.println(x);
+        System.out.println((end-start)/1000000000);
+
+        start = System.nanoTime(); 
+        for(int i = 0; i <= 100000000;i++){
+            charArrayList.add(Character.valueOf((char) (rand.nextInt(26)+'a')));
+        }
+        end = System.nanoTime();
+        
+        //System.out.println(x);
         System.out.println((end-start)/1000000000);
     }
 }//315409818
