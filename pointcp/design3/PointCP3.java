8b8f3b79d1d48fc2d560058d697714863f90147b
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-02 19:22:33 -0400

    fixed getters and removed convert

--- pointcp/design3/PointCP3.java
+++ pointcp/design3/PointCP3.java
@@ -26,97 +26,46 @@ public class PointCP3
    * Contains the current value of X or RHO depending on the type
    * of coordinates.
    */
-  private double xOrRho;
+  private double x;
   
   /**
    * Contains the current value of Y or THETA value depending on the
    * type of coordinates.
    */
-  private double yOrTheta;
+  private double y;
 	
   
   //Constructors ******************************************************
 
   /**
-   * Constructs a coordinate object, with a type identifier.
+   * Constructs a coordinate object of type cartesian
    */
   public PointCP3(char type, double xOrRho, double yOrTheta)
   {
-    if(type != 'C' && type != 'P')
-      throw new IllegalArgumentException();
-    this.xOrRho = xOrRho;
-    this.yOrTheta = yOrTheta;
-    typeCoord = type;
+    if(type != 'C' && type != 'P'){throw new IllegalArgumentException();}
+    if(type == 'C'){
+      this.x = xOrRho;
+      this.y = yOrTheta;
+    }else{
+      this.x = (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
+      this.y = (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
+    }
+ 
+    this.typeCoord = 'C';
   }
 	
   
   //Instance methods **************************************************
  
  
-  public double getX()
-  {
-    if(typeCoord == 'C') 
-      return xOrRho;
-    else 
-      return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
-  }
+  public double getX(){return this.x;}
   
-  public double getY()
-  {
-    if(typeCoord == 'C') 
-      return yOrTheta;
-    else 
-      return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
-  }
+  public double getY(){return this.y;}
   
-  public double getRho()
-  {
-    if(typeCoord == 'P') 
-      return xOrRho;
-    else 
-      return (Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2)));
-  }
+  public double getRho(){return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))); }
   
-  public double getTheta()
-  {
-    if(typeCoord == 'P')
-      return yOrTheta;
-    else 
-      return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
-  }
+  public double getTheta(){return (Math.toDegrees(Math.atan2(yOrTheta, xOrRho))) ;}
   
-	
-  /**
-   * Converts Cartesian coordinates to Polar coordinates.
-   */
-  public void convertStorageToPolar()
-  {
-    if(typeCoord != 'P')
-    {
-      //Calculate RHO and THETA
-      double temp = getRho();
-      yOrTheta = getTheta();
-      xOrRho = temp;
-      
-      typeCoord = 'P';  //Change coord type identifier
-    }
-  }
-	
-  /**
-   * Converts Polar coordinates to Cartesian coordinates.
-   */
-  public void convertStorageToCartesian()
-  {
-    if(typeCoord != 'C')
-    {
-      //Calculate X and Y
-      double temp = getX();
-      yOrTheta = getY();
-      xOrRho = temp;
-   
-      typeCoord = 'C';	//Change coord type identifier
-    }
-  }
 
   /**
    * Calculates the distance in between two points using the Pythagorean
