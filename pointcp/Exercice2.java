aca9f733cb0b0122fbb05ca5c50ffc6c6c84f506
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-02 15:18:57 -0400

    started working on it

---
+++ pointcp/Exercice2.java
@@ -0,0 +1,13 @@
+
+public class Exercice2 {
+ArrayList<Char> acharArrayList = new ArrayList<Char>();
+//Char[] charArray = new Char[];
+
+    
+    public static void main (String args[]){
+        	long start = System.nanoTime();
+            while((System.nanoTime()- start)/1000000000<10){
+
+            }
+    }
+}
