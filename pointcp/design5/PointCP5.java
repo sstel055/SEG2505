9021371cb325244ac494083caeb4e3cd35648eb0
unknown <emaal080@uottawa.ca>
2020-10-03 15:21:10 -0400

    CP5

--- pointcp/design5/PointCP5.java
+++ pointcp/design5/PointCP5.java
@@ -1,3 +1,23 @@
 public abstract class PointCP5{
-  
+
+    public double getDistance(PointCP2 pointB){
+
+    }
+    
+    public double getY(){
+
+    }
+
+    
+    public double getRho(){
+
+    }
+
+    
+    public double getTheta(){
+
+    }
+    public PointCP rotatePoint(double rotation){
+
+    }
 }
