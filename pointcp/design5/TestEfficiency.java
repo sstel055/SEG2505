7b46719e9fff8e8f1047423732f17021ffdeca4c
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-03 18:26:11 -0400

    declaration loops almost done

--- pointcp/design5/TestEfficiency.java
+++ pointcp/design5/TestEfficiency.java
@@ -1,15 +1,40 @@
 package design5;
-
+import java.util.Random;
 public class TestEfficiency {
     public static void main(String[] args) {
-        PointCP[] cpArray = new PointCP[1000];
-        PointCP5 abc = new PointCP3('C', 1, 1);
+        int numberOfPoints = 1000;
+        PointCP[] cpArray = new PointCP[numberOfPoints];
+        PointCP2[] cpArray2 = new PointCP2[numberOfPoints];
+        PointCP3[] cpArray3 = new PointCP3[numberOfPoints];
+        Random r = new Random();
+        String cOrP = "CP";
 
 
-        //Declare 1000 Points 
-        for(int i = 0; i<1000;i++){
-
-
+        //Declarartion for PointCP 
+        long start = System.nanoTime();
+        for(int i = 0; i<numberOfPoints;i++){
+            cpArray[i] = new PointCP((char)cOrP.charAt(r.nextInt(2)) , r.nextInt(1001), (double) r.nextInt(361));
         }
+        long end = System.nanoTime();
+        System.out.println("Average PointCP Declaration time: "+String.valueOf(((end-start)/1000000000)/numberOfPoints));
+
+
+
+        //Declaration for PointCP2 
+        start = System.nanoTime();
+        for(int i = 0; i<numberOfPoints;i++){
+            cpArray2[i] = new PointCP2((char)cOrP.charAt(r.nextInt(2)) , r.nextInt(1001), (double) r.nextInt(361));
+        }
+        end = System.nanoTime();
+
+
+
+        //Declaration for PointCP3 
+        for(int i = 0; i<numberOfPoints;i++){
+            cpArray3[i] = new PointCP3((char)cOrP.charAt(r.nextInt(2)) , r.nextInt(1001), (double) r.nextInt(361));
+        }
+
+
+
     }
 }
