259910564bcf26fe9fc816994c4e36c1aecb1386
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-03 18:02:19 -0400

    Made test efficiency additions

--- pointcp/design5/TestEfficiency.java
+++ pointcp/design5/TestEfficiency.java
@@ -2,6 +2,14 @@ package design5;
 
 public class TestEfficiency {
     public static void main(String[] args) {
+        PointCP[] cpArray = new PointCP[1000];
         PointCP5 abc = new PointCP3('C', 1, 1);
+
+
+        //Declare 1000 Points 
+        for(int i = 0; i<1000;i++){
+
+
+        }
     }
 }
