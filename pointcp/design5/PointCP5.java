10215f5dcd024b082eb07045da626e8c84ed4ca5
unknown <emaal080@uottawa.ca>
2020-10-03 15:28:47 -0400

    aids and cancer

--- pointcp/design5/PointCP5.java
+++ pointcp/design5/PointCP5.java
@@ -2,24 +2,31 @@ package design5;
 
 public abstract class PointCP5{
 
-    public double getDistance(PointCP2 pointB){
+    public double getDistance(PointCP2 pointB){.
+         // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double x = getX();
+    double deltaX = x - pointB.getX();
+    double deltaY = x - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
 
     }
     
-    public double getY(){
+    public abstract double getY(){
 
     }
 
     
-    public double getRho(){
+    public abstract double getRho(){
 
     }
 
     
-    public double getTheta(){
+    public abstract double getTheta(){
 
     }
-    public PointCP rotatePoint(double rotation){
+    public abstract PointCP rotatePoint(double rotation){
 
     }
 
