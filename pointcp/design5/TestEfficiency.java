b68f95d8c3f5a587d35f4673c7c56b4720b5a0d9
Ricardo Saikali <ricardo_saikali@hotmail.com>
2020-10-04 16:17:42 -0400

    chnaged to 500000

--- pointcp/design5/TestEfficiency.java
+++ pointcp/design5/TestEfficiency.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class TestEfficiency {
     public static void main(String[] args) {
 
-        int numberOfPoints = 1000000;
+        int numberOfPoints = 500000;
         PointCP5[] cp5Array = new PointCP5[numberOfPoints];
         PointCP[] cpArray = new PointCP[numberOfPoints];
         char[] cp = new char[numberOfPoints];
