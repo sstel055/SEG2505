fcd5f021539c7bd3acaab5b1a129830af828676b
sstella <sstella2001@gmail.com>
2020-10-03 21:18:04 -0400

    Proper naming conventions

--- pointcp/design5/TestEfficiency.java
+++ pointcp/design5/TestEfficiency.java
@@ -10,43 +10,43 @@ public class TestEfficiency {
         PointCP5[] cp5Array = new PointCP5[numberOfPoints];
         PointCP[] cpArray = new PointCP[numberOfPoints];
         char[] cp = new char[numberOfPoints];
-        double[] theta = new double[numberOfPoints];
-        double[] rho = new double[numberOfPoints];
+        double[] yOrTheta = new double[numberOfPoints];
+        double[] xOrRho = new double[numberOfPoints];
         double[] angle = new double[numberOfPoints];
         Random r = new Random();
         String cOrP = "CP";
-        
-        //Declaring the arrays for cp, rho and theta so that results are consistent  
+
+        //Declaring the arrays for cp, xOrRho and yOrTheta so that results are consistent
         for (int i = 0; i<numberOfPoints; i++) {
             cp[i] = (char)cOrP.charAt(r.nextInt(2));
-            rho[i] = r.nextInt(1001);
-            theta[i]=(double) r.nextInt(361);
+            xOrRho[i] = r.nextInt(1001);
+            yOrTheta[i]=(double) r.nextInt(361);
             angle[i] = r.nextInt(91);
         }
 
 
         /////////////////////////////////////////////////////Declaration Section////////////////////////////////////////////////////
-        //Declarartion for PointCP 
+        //Declarartion for PointCP
         long start = System.nanoTime();
         for(int i = 0; i<numberOfPoints;i++){
-            cpArray[i] = new PointCP(cp[i], rho[i], theta[i]);
+            cpArray[i] = new PointCP(cp[i], xOrRho[i], yOrTheta[i]);
         }
         long end = System.nanoTime();
         System.out.println("PointCP Declaration time(ms): "+String.valueOf((end-start)/1000000));
 
-        
+
         //Declaration for PointCP5
         start = System.nanoTime();
         for(int i = 0; i<numberOfPoints;i++){
             if (cp[i] == 'P') {
-                cp5Array[i] = new PointCP2(cp[i], rho[i], theta[i]);
+                cp5Array[i] = new PointCP2(cp[i], xOrRho[i], yOrTheta[i]);
             } else {
-                cp5Array[i] = new PointCP3(cp[i], rho[i], theta[i]);
+                cp5Array[i] = new PointCP3(cp[i], xOrRho[i], yOrTheta[i]);
             }
         }
         end = System.nanoTime();
         System.out.println("PointCP5 Declaration time(ms): "+String.valueOf((end-start)/1000000));
-        
+
 
         ////////////////////////////////////////////////////////getX() Section//////////////////////////////////////////////////////////
         //getX() function test for PointCP
@@ -136,8 +136,8 @@ public class TestEfficiency {
         }
         end = System.nanoTime();
         System.out.println("PointCP5 getDistance time(ms): "+String.valueOf((end-start)/1000000));
-        
-        
+
+
         //////////////////////////////////////////////////////////rotatePoint() Section///////////////////////////////////////////
         //rotatePoint() function test for PointCP
         start = System.nanoTime();
@@ -155,7 +155,6 @@ public class TestEfficiency {
         end = System.nanoTime();
         System.out.println("PointCP5 rotatePoint time(ms): "+String.valueOf((end-start)/1000000));
 
-         
+
     }
 }
-
